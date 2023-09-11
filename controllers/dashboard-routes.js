d66f07d6634153abc70be328478dcc8d857db9fc
sthogue <sthogue16@gmail.com>
2023-09-10 23:07:38 -0500

    added dashboard-routes.js

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// GET all posts for dashboard
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: ['id', 'title', 'content', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include:{
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+        const posts = postData.map(post => post.get({ plain: true}));
+        res.render('dashboard', {
+            posts,
+            loggedIn: true
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// GET one post for dashboard
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try{
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Comment,
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    ]
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        const post = postData.get({ plain: true});
+        res.render('edit-post', {
+            post,
+            loggedIn: true
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
