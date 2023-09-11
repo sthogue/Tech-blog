c80e1fa84ad47eb7a5f6f757f571a3c9373167c4
sthogue <sthogue16@gmail.com>
2023-09-11 15:18:32 -0500

    cleaned up code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,33 +1,34 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
-router.get('/', (req, res) => {
-    Post.findAll({
-        attributes: ['id', 'title', 'content', 'created_at'],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'created_at'],
-                include:{
-                    model: User,
-                    attributes: ['username']
-                }
+router.get('/', withAuth, async (req, res) => {
+
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: ['id', 'title', 'content', 'created_at'],
+            include:[
+                {
+                model: User,
+                attributes: ['username']
             },
             {
-                model: User,
+                module: User,
                 attributes: ['username']
             }
         ]
-    }).then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true}));
-        res.render('homepage', {
-            posts,
-            loggedIn: req.session.loggedIn
         });
-    }).catch(err => {
+        const posts = postData.map(post => post.get({ plain: true}));
+        res.render('dashboard', {
+            posts,
+            loggedIn: true
+        });
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
-    });
+    }
 });
 
 router.get('/login', (req, res) => {
@@ -55,14 +56,14 @@ router.get('/post/:id', async (req, res) => {
                     include: [
                         {
                             model: User,
-                            attributes: ['username']
+                            attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                            include: {
+                                model: User,
+                                attributes: ['username']
+                            }
                         }
                     ]
                 },
-                {
-                    model: User,
-                    attributes: ['username']
-                }
             ]
         });
         if (!postData){
