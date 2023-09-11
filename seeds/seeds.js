8644607ca96c8abdb3e2802427dbe827a4046412
sthogue <sthogue16@gmail.com>
2023-09-11 11:42:02 -0500

    updated to seed comment data

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -3,6 +3,7 @@ const { User, Post } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -13,6 +14,8 @@ const seedDatabase = async () => {
     });
 
     await Post.bulkCreate(postData);
+
+    await Comment.bulkCreate(commentData);
 
     process.exit(0);
 }
